056623734b51baf5d2fb73a6f515503da2f7cec0
Abenezer Fikadu <62052771+Abenezerfikadu@users.noreply.github.com>
2022-10-19 14:43:24 +0300

    Add files via upload

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -21,8 +21,10 @@ int print_last_digit(int n);
 
 void jack_bauer(void);
 
+void times_table(void);
+
 int add(int a, int b);
 
 void print_to_98(int n);
 
-void times_table(int n);
+void print_times_table(int n);
